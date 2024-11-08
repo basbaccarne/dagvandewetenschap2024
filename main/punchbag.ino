e7dc064ac42140f9bc49f8371db2f25f8ad95362
Bas Baccarne <bastiaan.baccarne@ugent.be>
2024-11-08 16:50:33 +0100

    toms update

--- main/punchbag.ino
+++ main/punchbag.ino
@@ -45,33 +45,49 @@ In entails the following components:
   * (-) side velostat to side 1 of resistor (150Ω, brown/green/brown)) 
 */
 
-// libraries
+/*
+  Arduino LSM6DS3 - Simple Accelerometer
+
+  This example reads the acceleration values from the LSM6DS3
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Uno WiFi Rev 2 or Arduino Nano 33 IoT
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+
+#include <Arduino_LSM6DS3.h>
 #include <FastLED.h>
 #include <ArduinoBLE.h>
-#include "DFRobotDFPlayerMini.h"
+
+
+// pins
+#define ledPin 7
 
 // define the BLE service
 BLEService punchService("95ff7bf8-aa6f-4671-82d9-22a8931c5387");
 BLEFloatCharacteristic punch("95ff7bf8-aa6f-4671-82d9-22a8931c5387", BLERead | BLENotify);
 
-// pins
-#define ledPin 7
-#define sensorPin A0
-#define DFSerial Serial1 // On Arduino Nano 33IOT the TX & RX pins are added to the Serial1 object
+// Variables to save acceleration and gyro values
+float ax, ay, az;
 
-// configuration variables
-const int lower_force_threshold = 200; // force threshold to light up the first LED (check serial to set right value) aka punch trehsold
-const long ledInterval = 0; // Set the interval between lighting up the LEDs
-const long sensorInterval = 10; // Set the interval between reading the sensor (timer instead of delay)
-const int volume = 30; //Set volume value. From 0 to 30
-const int sound_duration = 300; // allow the sound to play > could be optimized with a switch case
+// Variables to store initial calibration values for acceleromter
+float ax_start, ay_start, az_start;
 
-// timers (to avoid delay)
-unsigned long previousMillis = 0;
-unsigned long previousMillis_soundFX = 0;
-
-// sensor readings
-int sensorReading = 0;
+float punching_bag_mass = 25; // Mass in kg
+float gravity_constant = 9.81; // m/s²
+float range_contant = 4; // Range setting of the ACC
+float gravity_range = gravity_constant * range_contant; // Full (positive) range of ACC in m/s²
+float register_range = pow(2, 15); // Full (positive) register range of the ACC register (16bit)
+float force = 0;
+const int lower_force_threshold = 100; // force threshold to light up the first LED (check serial to set right value) aka punch trehsold
+const int upper_force_threshold = 400;
+const long ledInterval = 50; // Set the interval between lighting up the LEDs
 
 // LED strip variables
 #define ledCount 120
@@ -80,14 +96,27 @@ int brightness = 100; // Set brightness level (0-255) - check for amps required
 int currentLitLEDs = 0; // The number of LEDs currently lit up
 int targetLitLEDs = 0; // The number of LEDs that should be lit up
 
-// DF player
-DFRobotDFPlayerMini myDFPlayer; // Create the DF player object
 
 void setup() {
-
-  // initialize Serial communication
   Serial.begin(9600);
-  DFSerial.begin(9600); // inititialize RX/TX communication for the DF player
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax_start, ay_start, az_start);
+  }
 
   // initialize led strip
   FastLED.addLeds<NEOPIXEL, ledPin>(leds, ledCount);
@@ -95,7 +124,7 @@ void setup() {
   FastLED.clear();
   FastLED.show();
 
-  // initialize BLE: part 1
+    // initialize BLE: part 1
   if (!BLE.begin())
     {
         Serial.println("BLE failed to Initiate");
@@ -119,72 +148,81 @@ void setup() {
   BLE.advertise();
   Serial.println("BLE sending device 1 is now advertising ...");
 
-  // initialize DF player
-  myDFPlayer.begin(DFSerial);
-  myDFPlayer.volume(volume); 
 }
 
-void loop() {
-  // get actual millis
-  unsigned long currentMillis = millis();
+int punches = 0;
 
-  // read sensor value & map it to the number of LEDs to light up
-  if (currentMillis - previousMillis >= sensorInterval) {
-    previousMillis = currentMillis;
-    // read the sensor value
-    sensorReading = analogRead(sensorPin);
-    Serial.print(sensorReading);
-    // map the sensor value to the number of LEDs to light up
-    targetLitLEDs = map(sensorReading, lower_force_threshold, 1023, 0, ledCount / 2);
+void loop() {
+  float ax, ay, az;
+
+      // Listen for BLE central
+      BLEDevice central = BLE.central();
+      if (central) {
+        Serial.print("Connected to central: ");
+        Serial.println(central.address());
+        if (!central.connected()) {
+          Serial.println("Disconnected from central");
+        }
+        while (central.connected()){
+          if (IMU.accelerationAvailable()) {
+            IMU.readAcceleration(ax, ay, az);
+
+            float prev_force = force;
+            float force = punching_bag_mass * sqrt(pow((ax-ax_start)*gravity_constant, 2) + pow((ay-ay_start)*gravity_constant, 2) + pow((az-az_start)*gravity_constant, 2));
+            // Filter using exponential moving average
+            force = 0.5*force + 0.5*prev_force;
+
+            updateLEDS(force);
+
+            // If force is above threshold calculate average force over punch duration
+            if (force > lower_force_threshold){
+
+              float totalforce = 0;
+              float readValues = 0;
+              // Keep reading values until force is below threshold
+              while (force > lower_force_threshold){
+                totalforce += force;
+                readValues++;
+                IMU.readAcceleration(ax, ay, az);
+                prev_force = force;
+                force = punching_bag_mass * sqrt(pow((ax-ax_start)*gravity_constant, 2) + pow((ay-ay_start)*gravity_constant, 2) + pow((az-az_start)*gravity_constant, 2));
+              }
+
+              force = totalforce/readValues;
+              Serial.println("punch with force: " + String(force));
+              punch.writeValue(force);
+              
+              punches++;
+              Serial.println("punches: " + String(punches));
+              delay(5);
+            } 
+          }
+        }
+
+      }
+  
+  // Poll for BLE events (this processes any pending BLE events)
+  BLE.poll();
+}
+
+void updateLEDS(float force){
+  // map the sensor value to the number of LEDs to light up
+    targetLitLEDs = map(force, 20, 300, 0, ledCount / 2);
     if (targetLitLEDs < 0) {
       targetLitLEDs = 0;
     }
-    // debugging prints
-    Serial.print(",");
-    Serial.print(targetLitLEDs);
-    int leftLeds = ledCount / 2 + targetLitLEDs;
-    Serial.print(",");
-    Serial.print(leftLeds);
-    int rightLeds = ledCount / 2 - targetLitLEDs;
-    Serial.print(",");
-    Serial.println(rightLeds);
-
-    // send the sensor value over BLE
-    punch.writeValue(sensorReading);
-  }
-
-  // If the force value is increasing, go up fast
-  if (targetLitLEDs > currentLitLEDs) {
-    currentLitLEDs = targetLitLEDs;
-    updateLEDStrip(currentLitLEDs);
-  }
-
-  // If the force value is decreasing, go down slowly
-  else if (targetLitLEDs < currentLitLEDs) {
-    currentLitLEDs--;  // Go down one LED at a time (slowly)
-    updateLEDStrip(currentLitLEDs);
-    delay(ledInterval);  // Slow down the decrease
-  }
-
-  // DF player: play the second audio file if the sensor value is above punch threshold (lower_force_threshold)
-  // delay 100 ms to allow the sound to play
-  if (sensorReading > lower_force_threshold && currentMillis - previousMillis_soundFX >= sound_duration ) {
-    previousMillis_soundFX = currentMillis;
-    myDFPlayer.play(2);
-    Serial.println("bliep!");
-  }
-
-  // BLE error handling
-  BLEDevice central = BLE.central();
-  if (central) {
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
-    if (!central.connected()) {
-      Serial.println("Disconnected from central");
+      // If the force value is increasing, go up fast
+    if (targetLitLEDs > currentLitLEDs) {
+      currentLitLEDs = targetLitLEDs;
+      updateLEDStrip(currentLitLEDs);
     }
-  }
-  // Poll for BLE events (this processes any pending BLE events)
-  BLE.poll();
+
+    // If the force value is decreasing, go down slowly
+    else if (targetLitLEDs < currentLitLEDs) {
+      currentLitLEDs--;  // Go down one LED at a time (slowly)
+      updateLEDStrip(currentLitLEDs);
+      delay(ledInterval);  // Slow down the decrease
+    }
 }
 
 void updateLEDStrip(int numLitLEDs) {
