2d09fa0250f067823cb5886b24661b49f0ee83ae
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-01-24 17:55:33 +0100

    Fix font path references to use relative paths

--- python/src/main.py
+++ python/src/main.py
@@ -31,8 +31,8 @@ screen_width = screen_info.current_w
 screen_height = screen_info.current_h
 
 # fonts
-FONT_PATH1 = ".dagvandewetenschap2024/python/data/Jost-VariableFont_wght.ttf"
-FONT_PATH2 = ".dagvandewetenschap2024/python/data/JosefinSans-VariableFont_wght.ttf"
+FONT_PATH1 = "./dagvandewetenschap2024/python/data/Jost-VariableFont_wght.ttf"
+FONT_PATH2 = "./dagvandewetenschap2024/python/data/JosefinSans-VariableFont_wght.ttf"
 
 font_H1 = pygame.font.Font(FONT_PATH1, 74)
 font_H2 = pygame.font.Font(FONT_PATH2, 74)
