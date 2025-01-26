124fc4d6abc4ad203afae8dcc5989f0612fcbc61
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-01-26 10:11:42 +0100

    Refactor main.py to improve structure and readability; added functions for loading spritesheet, managing scenes, and handling serial communication.

--- python/src/main.py
+++ python/src/main.py
@@ -7,44 +7,63 @@ import serial
 import time
 import sys
 import os
+from pathlib import Path
 import threading
-from PIL import Image
 
 # Initialize Pygame
 pygame.init()
 
-# Constants
-FRAMERATE = 60
-ANIMATION_SPEED = 200
+# CONSTANTS
+"""video settings"""
+FRAMERATE = 24
+screen_info = pygame.display.Info()
+screen_width = screen_info.current_w
+screen_height = screen_info.current_h
+pygame.mouse.set_visible(False)
 
+"""paths"""
+script_dir = Path(__file__).parent
+data_dir = script_dir.parent / 'data'
+spritesheet_path = data_dir / 'boxer_spritesheet.png'
+font1_path = data_dir / 'Jost-VariableFont_wght.ttf'
+font2_path = data_dir / 'JosefinSans-VariableFont_wght.ttf'
+
+"""design system"""
 BG_COLOR = (34, 34, 34)
 GRAY = (82, 82, 82)
 MEDIUM_GRAY = (138, 138, 138)
 LIGHTGRAY = (216, 216, 216)
 BRIGHT_YELLOW = (255, 230, 0)
 
-# Get the screen size
-screen_info = pygame.display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
+font_H1 = pygame.font.Font(font1_path, 100)
+font_H2 = pygame.font.Font(font2_path, 50)
 
-# Set up the display
-screen = pygame.display.set_mode((screen_width,screen_height), pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.SCALED, vsync=1)
-pygame.display.set_caption("PunchPal")
+"""animated sprite settings"""
+COL_COUNT = 14  # the spritesheet has 14 columns
+ROW_COUNT  = 9  # the spritesheet has 9 rows
+FPS = 24        # frames per second
+SPRITE_WIDTH = 195     # each frame is 195 pixels wide
+SPRITE_HEIGHT = 300    # each frame is 300 pixels high
+VALID_FRAMES = (COL_COUNT*ROW_COUNT) - 5 # the last 5 frames in the spriteheet are blank
 
+# FUNCTIONS
+"""Function to load a spritesheet"""
+def load_spritesheet(spritesheet_path):
+    """Load and preprocess frames from the spritesheet."""
+    spritesheet = pygame.image.load(spritesheet_path).convert_alpha()
+    frames = []
+    for row in range(ROW_COUNT):
+        for col in range(COL_COUNT):
+            frame_index = row * COL_COUNT + col
+            if frame_index >= VALID_FRAMES:
+                break
+            frame = spritesheet.subsurface(
+                pygame.Rect(col * SPRITE_WIDTH, row * SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT)
+            )
+            frames.append(frame)
+    return frames
 
-# fonts
-FONT_PATH1 = "./dagvandewetenschap2024/python/data/Jost-VariableFont_wght.ttf"
-FONT_PATH2 = "./dagvandewetenschap2024/python/data/JosefinSans-VariableFont_wght.ttf"
-
-font_H1 = pygame.font.Font(FONT_PATH1, 100)
-font_H2 = pygame.font.Font(FONT_PATH2, 50)
-
-# Hide the mouse cursor
-pygame.mouse.set_visible(False)
-
-# set-up the serial connection
-# Global variable to store the serial connection
+"""Function to set-up the serial connection"""
 ser = None
 def connect_serial():
     global ser
@@ -56,18 +75,9 @@ def connect_serial():
         except serial.SerialException:
             print("Failed to connect to serial port. Retrying...")
             time.sleep(2)
-        
-# Start the serial connection thread
-serial_thread = threading.Thread(target=connect_serial)
-serial_thread.daemon = True
-serial_thread.start()
 
-# Initialize an empty dictionary
+"""Function to parse the incoming serial data"""
 serial_dict = {}
-
-# initatialize scene variable
-scene = "BOOTING"
-
 def parseSerial(line):
     # Parse the incoming data from 'message||value' to dictionary
     if "||" in line:
@@ -75,107 +85,17 @@ def parseSerial(line):
         serial_dict[message] = value
         # print(f"Parsed: {serial_dict}")
 
-# Load the animated GIF
-gif_path = "./dagvandewetenschap2024/python/data/boxer.gif"
-gif = Image.open(gif_path)
-frames = []
-for frame in range(gif.n_frames):
-    gif.seek(frame)
-    frame_image = pygame.image.fromstring(gif.tobytes(), gif.size, gif.mode)
-    frame_image = frame_image.convert_alpha() 
-    frame_image.set_colorkey((255, 255, 255))
-    frames.append(frame_image)
-
-# Define scene functions (static)
-def booting():
-    # background
-    screen.fill(BG_COLOR)
-
-    # main title
-    text = font_H1.render("Syteem operationeel!", True, (MEDIUM_GRAY))
-    text_rect = text.get_rect()
-    text_rect.center = (screen_width // 2, screen_height // 2)
-    screen.blit(text, text_rect)
-
-    # subtitle
-    text = font_H2.render("Klop op de zak om verder te gaan", True, (BRIGHT_YELLOW))
-    text_rect = text.get_rect()
-    text_rect.center = (screen_width // 2, screen_height // 2 + 100)
-    screen.blit(text, text_rect)
-
-    pygame.display.update()
-
-def idle():
-    # background
-    screen.fill(BG_COLOR)
-
-    # main title
-    text = font_H1.render("Klop om de zak om te beginnen.", True, (MEDIUM_GRAY))
-    text_rect = text.get_rect()
-    text_rect.center = (screen_width // 2, screen_height // 4)
-    screen.blit(text, text_rect)
-
-    # animated gif
-    current_time = pygame.time.get_ticks()
-    frame_index = (current_time // ANIMATION_SPEED) % len(frames)
-    frame = frames[frame_index]
-    frame_rect = frame.get_rect(center=(screen_width // 2, screen_height // 2))
-    screen.blit(frame, frame_rect)
-
-    pygame.display.update()
-
-def challenge1():
-    screen.fill(MEDIUM_GRAY)
-    pygame.display.update()
-
-def challenge1_debrief():
-    screen.fill(LIGHTGRAY)
-    pygame.display.update()
-
-def challenge2():
-    screen.fill(BRIGHT_YELLOW)
-    pygame.display.update()
-
-def challenge2_debrief():
-    screen.fill(BG_COLOR)
-    pygame.display.update()
-
-def challenge3():
-    screen.fill(GRAY)
-    pygame.display.update()
-
-def challenge3_debrief():
-    screen.fill(MEDIUM_GRAY)
-    pygame.display.update()
-
-def conclusion():
-    screen.fill(LIGHTGRAY)
-    pygame.display.update()
-
-# initialization
-clock = pygame.time.Clock()
-
-# Initial background load
-screen.fill(BG_COLOR)
-pygame.display.update()
-
-# Main loop
-booting()
-running = True
-current_state = None
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            running = False
-
-     # Read from the serial port
+"""Function to read from the serial port and set variables"""
+def readSerial():
     try:
         if ser and ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
+            # uncomment to print raw serial input
             # print(line) 
+
             parseSerial(line)
 
+            # detect state changes
             if "state" in serial_dict:
                 new_state = serial_dict["state"]
                 scene = serial_dict["state"]
@@ -201,6 +121,7 @@ while running:
                     elif scene == "CONCLUSION":
                         conclusion()
 
+    # manage serial connection errors
     except (serial.SerialException, OSError) as e:
         print(f"Serial connection error: {e}")
         ser = None
@@ -209,14 +130,139 @@ while running:
         serial_thread.daemon = True
         serial_thread.start()
 
-    # scenes #
-    ##########
+# SCENE MANAGEMENT (static scene elements)
+"""Set initial scene"""
+scene = "BOOTING"
 
-    if scene == "BOOTING":
-        True
+"""Function to render the booting screen"""
+def booting():
+    # background
+    screen.fill(BG_COLOR)
+
+    # main title
+    text = font_H1.render("Syteem operationeel!", True, (MEDIUM_GRAY))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 2, screen_height // 2)
+    screen.blit(text, text_rect)
+
+    # subtitle
+    text = font_H2.render("Klop op de zak om verder te gaan", True, (BRIGHT_YELLOW))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 2, screen_height // 2 + 100)
+    screen.blit(text, text_rect)
+
+    pygame.display.flip()
+
+"""Function to render the idle screen"""
+def idle():
+    # background
+    screen.fill(BG_COLOR)
+
+    # main title
+    text = font_H1.render("Klop om de zak om te beginnen.", True, (MEDIUM_GRAY))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 2, screen_height // 4)
+    screen.blit(text, text_rect)
+
+    # Define position for the animated sprite
+    frame_x = (SCREEN_WIDTH - SPRITE_WIDTH) // 2
+    frame_y = (SCREEN_HEIGHT - SPRITE_HEIGHT) // 2 + 100
+    animation_rect = pygame.Rect(frame_x, frame_y, SPRITE_WIDTH, SPRITE_HEIGHT)
+
+    pygame.display.update()
+
+"""Function to render the challenge 1 screen"""
+def challenge1():
+    screen.fill(MEDIUM_GRAY)
+    pygame.display.update()
+
+""""Function to render the challenge 1 debrief screen"""
+def challenge1_debrief():
+    screen.fill(LIGHTGRAY)
+    pygame.display.update()
+
+"""Function to render the challenge 2 screen"""
+def challenge2():
+    screen.fill(BRIGHT_YELLOW)
+    pygame.display.update()
+
+"""Function to render the challenge 2 debrief screen"""
+def challenge2_debrief():
+    screen.fill(BG_COLOR)
+    pygame.display.update()
+
+"""Function to render the challenge 3 screen"""
+def challenge3():
+    screen.fill(GRAY)
+    pygame.display.update()
+
+"""Function to render the challenge 3 debrief screen"""
+def challenge3_debrief():
+    screen.fill(MEDIUM_GRAY)
+    pygame.display.update()
+
+"""Function to render the conclusion screen"""
+def conclusion():
+    screen.fill(LIGHTGRAY)
+    pygame.display.update()
+
+# SCENE MANAGEMENT (dynamic scene elements)
+def idle_dynamic():
+    # Clear the animation area by filling it with black
+    screen.fill((0, 0, 0), animation_rect)
+
+    # Draw the current frame of animation
+    screen.blit(frames[frame_index], (frame_x, frame_y))
+
+    # Update only the animated area (animation rectangle)
+    pygame.display.update(animation_rect)
+
+    # Move to the next frame
+    frame_index = (frame_index + 1) % len(frames)
+    
+# PRELOADING AND RENDERING: GENERAL DISPLAY
+"""Set up the display"""
+screen = pygame.display.set_mode((screen_width,screen_height), pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.SCALED, vsync=1)
+pygame.display.set_caption("PunchPal")
+
+# PRELOADING AND RENDERING: SERIAL COMMUNICATION
+"""Start the serial connection thread"""
+serial_thread = threading.Thread(target=connect_serial)
+serial_thread.daemon = True
+serial_thread.start()
+
+# PRELOADING AND RENDERING: ANIMATIONS
+"""Load the spritesheet frames"""
+frames = load_spritesheet(spritesheet_path)
+frame_index = 0
+
+# PRELOADING AND RENDERING: CLOCK, INIT SCREEN & LOOP
+"""initialize clock"""
+clock = pygame.time.Clock()
+
+"""load first scene"""
+booting()
+
+# MAIN LOOP
+running = True
+current_state = scene
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
+
+    # Read from the serial port 
+    # & trigger static scene management
+    readSerial()
+    
+    # Dynamic scene management
+    if scene == "BOOTING": # the booting screen is static
+        True 
     
     elif scene == "IDLE":
-        idle()
+        idle_dynamic()
         
     elif scene == "CHALLENGE1":
         # main title
