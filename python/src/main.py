b933ff862781b98da51100f162f1a11b68c6ba28
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-01-26 10:20:09 +0100

    Add global declaration for serial port in readSerial function

--- python/src/main.py
+++ python/src/main.py
@@ -87,6 +87,7 @@ def parseSerial(line):
 
 """Function to read from the serial port and set variables"""
 def readSerial():
+    global ser 
     try:
         if ser and ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
